4d15b9d77fd1a10b5dfb2957bc12e7fb84d0f534
arpanshettihalli <shaivaarpan2006@gmail.com>
2025-05-16 20:00:20 +0530

    addded program1.cpp

---
+++ program1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+int main()
+{
+    std::cout<<"hello world"<<'\n';
+    std::cout<<"my name is arpan"<<'\n';
+    char c='a'+3;
+    int a=34.4+c;
+    std::cout<<char(a)<<std::endl;
+    std::string sentence= "arpan";
+    std::string beac="is";
+    std::cout<<sentence<<'\n'<<beac;
+    return 0;
+}
