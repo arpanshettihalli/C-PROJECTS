93962efc4c63a9fd3c628d24cb0febc5bac94f7e
arpanshettihalli <shaivaarpan2006@gmail.com>
2025-05-19 19:50:26 +0530

    modified program1.cpp

--- program1.cpp
+++ program1.cpp
@@ -1,13 +1,24 @@
 #include<iostream>
+namespace rainterrain_asphalt{
+    int x=34;
+}
+namespace asphalt{
+    int x=34;
+}
+namespace mud{
+    int x=34;
+}
 int main()
-{
+{   int x=3;
     std::cout<<"hello world"<<'\n';
     std::cout<<"my name is arpan"<<'\n';
     char c='a'+3;
     int a=34.4+c;
     std::cout<<char(a)<<std::endl;
+    const double pi=3.14;
     std::string sentence= "arpan";
     std::string beac="is";
-    std::cout<<sentence<<'\n'<<beac;
+    std::cout<<sentence<<'\n'<<beac<<'\n';
+    std::cout<<asphalt::x;
     return 0;
 }
